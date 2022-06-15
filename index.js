31dbd4980403bb660d6cc44e13d7e539a080ee59
oyayafelix <oyayafelix@gmail.com>
2022-06-15 09:54:12 +0300

    solution

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+
+input = document.getElementById('input');
+
 function addingEventListener() {
+    alert('Congrats, this button was successfully clicked!');
+    input.addEventListener('click', addingEventListener);
 }
